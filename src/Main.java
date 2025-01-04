0377bc35255c54f82aaac37dce546c59b568771d
CristianMarNav <cristianmartineznavarro4@gmail.com>
2025-01-04 12:12:26 +0100

    Commit 1: Clase Main creada con mensaje básico de bienvenida.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Clase principal para ejecutar el juego "Guess the Movie".
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Guess the Movie!");
+        System.out.println("¡Buena suerte!\n");
+    }
+}
